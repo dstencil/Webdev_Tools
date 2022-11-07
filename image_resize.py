1dc5140c42acdaaf443f7ef7d6935febf79d2c17
dstencil <35854396+dstencil@users.noreply.github.com>
2022-11-07 19:05:40 +0000

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -2,8 +2,8 @@ from PIL import Image
 
 filepath = str(input('Enter path of image to resize: '))
 savepath = str(input('Enter path to save image: '))
-width = int(input("Enter width"))
-height = int(input("Enter Height"))
+width = int(input("Enter width: "))
+height = int(input("Enter Height: "))
 
 
 with Image.open (filepath) as im:
